0edefc15ae5b5c32bdcd69602156f068dec132f8
Aria <aria.bonczek@gmail.com>
2015-09-13 06:27:32 -0400

    Learned how awful loading and storing for every operation is, added the LVector class to provide a basic interface for doing it the right way. Will go back and fix old vector code. Gotta be more data-oriented

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include "Vector2.h"
 #include "Vector3.h"
 #include "Vector4.h"
+#include "LVector.h"
 
 using namespace LMath;
 
@@ -21,13 +22,25 @@ void scalar(Vector2& v1, Vector2& v2)
 	}
 }
 
+void loaded(VECTOR& v1, VECTOR v2)
+{
+	for (int i = 0; i < 100000; i++)
+	{
+		LVector::Add(v1, v2);
+	}
+}
+
 int main(int argc, char** argv)
 {
 	Vector2 v1(1.0f, 1.0f);
 	Vector2 v2(1.0f, 1.0f);
 
+	VECTOR v1v = LVector::Load(&v1.x);
+	VECTOR v2v = LVector::Load(&v2.x);
+
 	oper(v1, v2);
 	scalar(v1, v2);
+	loaded(v1v, v2v);
 
 	return 0;
 }	
